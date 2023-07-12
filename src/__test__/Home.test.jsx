8842b1497890aef2b8ba391448a217654d197ad4
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-12 14:30:32 +0545

    Test file for NavBar component

--- src/__test__/Home.test.jsx
+++ src/__test__/Home.test.jsx
@@ -4,16 +4,16 @@ import { render, screen } from '@testing-library/react';
 import Home from '../components/Home';
 
 describe('Home component', () => {
-    test('renders home component', () => {
-        render(<Home />);
-        screen.debug();
-    });
+  test('renders home component', () => {
+    render(<Home />);
+    screen.debug();
+  });
 
-    test('renders correctly', () => {
-        const component = renderer.create(
-          <Home />,
-        );
-        const tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-      });
+  test('renders correctly', () => {
+    const component = renderer.create(
+      <Home />,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
